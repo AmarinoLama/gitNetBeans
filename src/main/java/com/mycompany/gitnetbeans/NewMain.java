28b35824685c34aa8d66fac2f23e5d2f2a35eb35
AmarinoLama <amarinolama@gmail.com>
2024-05-10 10:19:17 +0200

    feat () : cambiar clase

--- src/main/java/com/mycompany/gitnetbeans/NewMain.java
+++ src/main/java/com/mycompany/gitnetbeans/NewMain.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.gitnetbeans;
-
+ 
 public class NewMain {
 public static void main(String[] args) {
 System.out.println(Sumador.sumar (15f,4f));
